45bcc809894e4cc0c78a631f626a67f37e4d9840
Julian Orth <ju.orth@gmail.com>
2020-05-27 23:13:45 +0200

    a

--- uapi/tests/chdir.rs
+++ uapi/tests/chdir.rs
@@ -4,13 +4,15 @@ use uapi::*;
 #[test]
 fn chdir1() {
     let tmp = Tempdir::new();
+    let tmpdir = &std::fs::canonicalize(tmp.bstr()).unwrap();
 
-    let mut buf = [0; 1024];
-    let old = getcwd(&mut buf).unwrap();
+    let mut buf1 = [0; 1024];
+    let mut buf2 = [0; 1024];
+    let old = getcwd(&mut buf1).unwrap();
 
     chdir(&tmp).unwrap();
 
-    assert_eq!(tmp.bstr(), &*std::env::current_dir().unwrap());
+    assert_eq!(tmpdir, &*std::env::current_dir().unwrap());
 
     chdir(old).unwrap();
 
@@ -18,5 +20,5 @@ fn chdir1() {
 
     fchdir(*open(&tmp, c::O_RDONLY, 0).unwrap()).unwrap();
 
-    assert_eq!(tmp.bstr(), &*std::env::current_dir().unwrap());
+    assert_eq!(tmpdir, &*std::env::current_dir().unwrap());
 }
