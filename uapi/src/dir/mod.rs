65f94221e82f14933b31326af8005a9b0359f911
Julian Orth <ju.orth@gmail.com>
2020-05-28 15:34:02 +0200

    a

--- uapi/src/dir/mod.rs
+++ uapi/src/dir/mod.rs
@@ -10,6 +10,7 @@ pub fn opendir<'a>(name: impl IntoUstr<'a>) -> Result<Dir> {
     let name = name.into_ustr();
     let dir = unsafe { c::opendir(name.as_ptr()) };
     if dir.is_null() {
+
         Err(Errno::default())
     } else {
         Ok(Dir { dir })
