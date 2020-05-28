436972fcd9a06b61976ef79cf4c312ab785f0390
Julian Orth <ju.orth@gmail.com>
2020-05-28 10:30:12 +0200

    a

--- uapi/tests/mknodat_mode.rs
+++ uapi/tests/mknodat_mode.rs
@@ -7,14 +7,16 @@ use uapi::*;
 #[cfg(not(target_os = "macos"))]
 fn mknodat1() {
     const MODE: c::mode_t = 0o712;
+    const DEV: c::dev_t = 999;
 
     let f = || {
         let tmp = Tempdir::new();
-        let fd = open(&tmp, c::O_PATH, 0).unwrap();
+        let fd = open(&tmp, c::O_RDONLY, 0).unwrap();
         let path = format_ustr!("{}/a", tmp);
-        mknodat(*fd, "a", c::S_IFCHR | MODE, makedev(1, 2)).unwrap();
+        mknodat(*fd, "a", c::S_IFCHR | MODE, DEV).unwrap();
         let stat = stat(&path).unwrap();
-        assert_eq!(stat.st_rdev, makedev(1, 2));
+        assert_eq!(stat.st_rdev, DEV);
+        assert_eq!(stat.st_mode & c::S_IFMT, DEV);
         metadata(path.as_path()).unwrap().permissions().mode() & 0o777
     };
 
