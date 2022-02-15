21a6207a967f0ddf013129a2dc0aa0c365fc7cb6
Julian Orth <ju.orth@gmail.com>
2022-02-15 17:41:05 +0100

    Add support for pidfds

--- uapi-proc/src/lib.rs
+++ uapi-proc/src/lib.rs
@@ -40,6 +40,7 @@ lazy_static! {
             tc.linux_5_2 = major > 5 || (major == 5 && minor >= 2);
             tc.linux_5_6 = major > 5 || (major == 5 && minor >= 6);
             tc.linux_5_9 = major > 5 || (major == 5 && minor >= 9);
+            tc.linux_5_10 = major > 5 || (major == 5 && minor >= 10);
         }
 
         tc
@@ -53,6 +54,7 @@ struct TestConditions {
     linux_5_2: bool,
     linux_5_6: bool,
     linux_5_9: bool,
+    linux_5_10: bool,
 }
 
 impl Parse for TestConditions {
@@ -67,6 +69,7 @@ impl Parse for TestConditions {
                 "linux_5_2" => tc.linux_5_2 = true,
                 "linux_5_6" => tc.linux_5_6 = true,
                 "linux_5_9" => tc.linux_5_9 = true,
+                "linux_5_10" => tc.linux_5_10 = true,
                 n => {
                     return Err(syn::Error::new(
                         name.span(),
@@ -93,7 +96,8 @@ pub fn test_if(
         || (tc.linux_4_16 && !TC.linux_4_16)
         || (tc.linux_5_2 && !TC.linux_5_2)
         || (tc.linux_5_6 && !TC.linux_5_6)
-        || (tc.linux_5_9 && !TC.linux_5_9);
+        || (tc.linux_5_9 && !TC.linux_5_9)
+        || (tc.linux_5_10 && !TC.linux_5_10);
     #[allow(clippy::match_bool)] // already disabled upstream
     let ignore = match ignore {
         false => quote!(),
--- uapi/tests/main/process/linux.rs
+++ uapi/tests/main/process/linux.rs
@@ -1,3 +1,6 @@
+use libc::O_NONBLOCK;
+use proc::test_if;
+use std::io::{Read, Write};
 use uapi::*;
 
 #[test]
@@ -5,4 +8,21 @@ fn gettid_() {
     let tid = read_link_to_new_ustring(c::AT_FDCWD, "/proc/thread-self").unwrap();
 
     assert_eq!(tid, format!("{}/task/{}", getpid(), gettid()));
+}
+
+#[test_if(linux_5_10)]
+fn pidfd_open_nonblock() {
+    let pidfd = pidfd_open(getpid(), 0).unwrap();
+    assert!(fcntl_getfl(pidfd.raw()).unwrap() & O_NONBLOCK == 0);
+    let pidfd = pidfd_open(getpid(), c::PIDFD_NONBLOCK).unwrap();
+    assert!(fcntl_getfl(pidfd.raw()).unwrap() & O_NONBLOCK != 0);
+    let (mut read, write) = pipe().unwrap();
+    let mut write_clone = pidfd_getfd(pidfd.raw(), write.raw(), 0).unwrap();
+    assert!(write_clone.raw() != write.raw());
+    write!(write_clone, "ayo").unwrap();
+    drop(write_clone);
+    drop(write);
+    let mut s = String::new();
+    read.read_to_string(&mut s).unwrap();
+    assert_eq!(s, "ayo");
 }
